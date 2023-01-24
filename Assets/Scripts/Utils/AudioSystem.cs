e4dc1fcb003fc4cfb3a43109db56c61e03d59c06
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2023-01-24 15:51:18 -0500

    fix an issue

--- Assets/Scripts/Utils/AudioSystem.cs
+++ Assets/Scripts/Utils/AudioSystem.cs
@@ -8,6 +8,9 @@ namespace Game.Utils
 
         public static AudioSource PlaySound(AudioClip _sound, Vector2 _position, float _volume, int _priority)
         {
+            if (_sound == null)
+                return null;
+
             GameObject soundObj = new GameObject("Sound", typeof(AudioSource), typeof(DestroyAfter));
             AudioSource au = soundObj.GetComponent<AudioSource>();
             DestroyAfter des = soundObj.GetComponent<DestroyAfter>();

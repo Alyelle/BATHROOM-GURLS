0dae8b9e3eb9ef4c7f9b85336b538155668804c1
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2023-02-27 18:05:23 -0500

    dashing

--- Assets/Scripts/Mechanics/Player/PlayerMovement.cs
+++ Assets/Scripts/Mechanics/Player/PlayerMovement.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 1f;
+    public float dashSpeed = 1f;
 
     Vector2 movement;
+
+    ParticleSystem par;
 
     Animator anim;
 
     Rigidbody2D rb;
 
+    bool lockVel;
+
     private void Awake()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        par = GetComponentInChildren<ParticleSystem>();
+
+        lockVel = false;
     }
 
     private void Update()
@@ -22,9 +30,26 @@ public class PlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        rb.velocity = moveSpeed * movement;
+        movement.Normalize();
 
-        anim.SetInteger("X", Mathf.RoundToInt(movement.x));
-        anim.SetInteger("Y", Mathf.RoundToInt(movement.y));
+        if (!lockVel)
+        {
+            rb.velocity = moveSpeed * movement;
+
+            anim.SetInteger("X", Mathf.RoundToInt(movement.x));
+            anim.SetInteger("Y", Mathf.RoundToInt(movement.y));
+
+            if (Input.GetMouseButtonDown(1) && movement.magnitude != 0f)
+            {
+                lockVel = true;
+
+                rb.velocity = dashSpeed * movement;
+                if (par != null)
+                    par.Play();
+            }
+        }
+
+        if (rb.velocity.magnitude <= moveSpeed)
+            lockVel = false;
     }
 }

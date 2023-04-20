207586cd969be49b31cfb4895355fdb03491c3f9
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2023-04-20 18:28:09 -0400

    e

---
+++ Assets/Scripts/Mechanics/Systems/Projectile/ProjectileRichochet.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace Game.Projectile
+{
+    public class ProjectileRichochet : ProjectileAddon
+    {
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            RaycastHit2D[] r = Physics2D.RaycastAll(transform.position, transform.up, proj.proj.Speed);
+            Debug.DrawRay(transform.position, transform.up, Color.white, proj.proj.Speed);
+            if (r.Length > 0)
+                foreach (RaycastHit2D rs in r)
+                {
+                    Vector2 vec = Vector2.Reflect(transform.up, rs.normal);
+                    float rot = Mathf.Atan2(-vec.x, vec.y) * Mathf.Rad2Deg;
+                    transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, rot));
+                    break;
+                }
+        }
+    }
+}

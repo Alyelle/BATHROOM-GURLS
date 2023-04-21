0a6f8f2be37d38087909ab1c350b25bc012d4da2
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2023-04-21 18:59:23 -0400

    e

--- Assets/Scripts/Mechanics/Systems/Projectile/ProjectileRichochet.cs
+++ Assets/Scripts/Mechanics/Systems/Projectile/ProjectileRichochet.cs
@@ -7,12 +7,15 @@ namespace Game.Projectile
     {
         private void OnTriggerEnter2D(Collider2D collision)
         {
+            if (collision.transform.TryGetComponent(out EntityBase _) || collision.transform.TryGetComponent(out ProjectileBase _))
+                return;
+
             RaycastHit2D[] r = Physics2D.RaycastAll(transform.position, transform.up, proj.proj.Speed);
             Debug.DrawRay(transform.position, transform.up, Color.white, proj.proj.Speed);
             if (r.Length > 0)
                 foreach (RaycastHit2D rs in r)
                 {
-                    if (rs.transform.TryGetComponent(out EntityBase ent) || rs.transform.TryGetComponent(out ProjectileBase proj))
+                    if (rs.transform.TryGetComponent(out EntityBase _) || rs.transform.TryGetComponent(out ProjectileBase _))
                         continue;
 
                     Vector2 vec = Vector2.Reflect(transform.up, rs.normal);
